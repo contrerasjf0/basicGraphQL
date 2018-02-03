3c259cad13bacd37c9cc5b9a85d490700c0f49a4
Juan Francisco Esperanza Contreras <contrerasjf.0@gmail.com>
2018-02-03 17:02:41 -0600

    it was implemented the resolvers

--- schema.js
+++ schema.js
@@ -1,16 +1,70 @@
 const { makeExecutableSchema } = require('graphql-tools')
 
 const typeDefs = `
+  # Esto es un curso en el sistema
   type Curso {
     id: ID!
     titulo: String!
+    # Esta es la descripción del curso
+    descripcion: String!
+    profesor: Profesor
+    rating: Float
+    comentarios: [Comentario]
+  }
+
+  type Profesor {
+    id: ID!
+    nombre: String!
+    nacionalidad: String!
+    genero: Genero
+    cursos: [Curso]
+  }
+
+  enum Genero {
+    MASCULINO
+    FEMENINO
+  }
+
+  type Comentario {
+    id: ID!
+    nombre: String!
+    cuerpo: String!
   }
 
   type Query {
     cursos: [Curso]
+    profesores: [Profesor]
+    curso(id: Int): Curso
+    profesor(id: Int): Profesor
   }
 `
 
-const schema = makeExecutableSchema({ typeDefs })
+const resolvers = {
+  Query: {
+    cursos: () => {
+      return [{
+        id: 1,
+        titulo: 'Curso de GraphQL',
+        descripcion: 'Aprendiendo GraphQL'
+      }, {
+        id: 2,
+        titulo: 'Curso de PHP',
+        descripcion: 'Aprendiendo PHP'
+      }]
+    }
+  },
+  Curso: {
+    profesor: () => {
+      return {
+        nombre: 'Pablo'
+      }
+    }
+  }
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
 
 module.exports = schema
