70e020bbd4b76a807d845d0c79c068e71ef31783
Juan Francisco Esperanza Contreras <contrerasjf.0@gmail.com>
2018-02-03 23:43:34 -0600

    the sechemes were modularized

---
+++ schema/index.js
@@ -0,0 +1,22 @@
+const { makeExecutableSchema } = require('graphql-tools')
+const resolvers = require('./resolvers');
+const Curso = require('./Curso');
+const Profesor = require('./Profesor');
+
+const rootQuery = `
+  type Query {
+    cursos: [Curso]
+    profesores: [Profesor]
+    curso(id: Int): Curso
+    profesor(id: Int): Profesor
+  }
+`
+
+
+
+const schema = makeExecutableSchema({
+  typeDefs: [rootQuery , Curso, Profesor],
+  resolvers
+})
+
+module.exports = schema
